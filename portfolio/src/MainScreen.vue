e2ba080a0aace2c76246b78a19f0dc547a52ac59
yeehawtoast <cpsmith96@gmail.com>
2024-07-01 23:28:55 -0600

    about.vue

--- portfolio/src/MainScreen.vue
+++ portfolio/src/MainScreen.vue
@@ -1,23 +1,17 @@
 <script setup lang="ts">
 //import { RouterLink, RouterView } from 'vue-router'
-import Hero from './components/Hero.vue';
-import Portfolio from './components/Portfolio.vue';
-import Contact from './components/Contact.vue';
+//import Hero from './components/Hero.vue';
+//import Portfolio from './components/Portfolio.vue';
+//import Contact from './components/Contact.vue';
 import About from './components/About.vue';
-import Resume from './components/Resume.vue';
+//import Resume from './components/Resume.vue';
 </script>
 
 <template>
   <header>
-  <Hero/>
-  <Portfolio/>
-  <Contact/>
+
   <About/>
-  <Resume/>
   </header>
 </template>
 <style>
-html{
-  background-image: url('./assets/background.svg');
-}
 </style>
